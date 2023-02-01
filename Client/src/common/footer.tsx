048c722b92b5177324db4d7666380ca281c390fe
kappa <praveennkappa@gamil.com>
2023-02-01 14:15:31 +0530

    Updated footer

--- Client/src/common/footer.tsx
+++ Client/src/common/footer.tsx
@@ -1,9 +1,46 @@
-
 function Footer() {
-    return (
-      <h1>Footer Section</h1>
-    );
-  }
-  
-  export default Footer;
-  
+  return (
+    <div className="container-fluid">
+      <div className="bg-dark text-white row">
+        <div className="nav-link text-center col-xs-12">Other Links</div>
+        <div className="col-xs-12">
+          <div className="container">
+            <div className="row">
+              <div className="nav-item col-xs-12 col-sm-4 col-md-2 text-center">
+                <a href="#" className="nav-link text-muted ">
+                  HR
+                </a>
+              </div>
+              <div className="nav-item col-xs-12 col-sm-4 col-md-2 text-center">
+                <a href="#" className="nav-link text-muted">
+                  MRN
+                </a>
+              </div>
+              <div className="nav-item col-xs-12 col-sm-4 col-md-2 text-center">
+                <a href="#" className="nav-link text-muted">
+                  CLARITY
+                </a>
+              </div>
+              <div className="nav-item col-xs-12 col-sm-4 col-md-2 text-center">
+                <a href="#" className="nav-link text-muted">
+                  TECH OPS
+                </a>
+              </div>
+              <div className="nav-item col-xs-12 col-sm-4 col-md-2 text-center">
+                <a href="#" className="nav-link text-muted">
+                  PMS
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-center">
+          A Tech Product. Copyright © 2023 Tech Technologies, Inc. All Rights
+          Reserved.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
